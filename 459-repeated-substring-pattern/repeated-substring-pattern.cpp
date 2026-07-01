001ced7e335a194fcfbef5e413b136b14781e68c
project-cell <pihuc250@gmail.com>
2026-07-01 23:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (71.79%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        return t.substr(1, t.size() - 2).find(s) != string::npos;     
+        
+    }
+};
